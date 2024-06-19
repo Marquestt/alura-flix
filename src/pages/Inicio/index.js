1aa5c2cd9c0d29b7498e39efe859c041484a5e6a
Marquestt <thatyanecmarques98@live.com>
2024-06-19 14:04:48 -0300

    Página inicial criada e sendo desenvolvida, adicionado uma tag header e uma h1

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,11 @@
+import styles from './Inicio.module.css';
+
+const Inicio = () => {
+    return(
+        <header className={styles.cabecalho}>
+            <h1>Olá mundo!</h1>
+        </header>
+    )
+}
+
+export default Inicio;
